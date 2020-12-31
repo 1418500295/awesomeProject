de0f5f62fa23b5cffb6f32ea1134dca246f65e52
1418500295 <45054883+1418500295@users.noreply.github.com>
2020-12-31 16:42:11 +0400

    Update one.go

--- cases/one.go
+++ cases/one.go
@@ -4,63 +4,72 @@ import (
 	"fmt"
 	"github.com/kirinlabs/HttpRequest"
 	"strings"
+	"sync"
 	"time"
 )
 
 var (
-	num =  10000
+
+	wg = sync.WaitGroup{}
+	num =  500
 	usedTime = 0
 	ok_num = 0
 	fal_num = 0
+	start_time = 0
+	end_time = 0
 )
 //接口压测实例
 func main(){
-	start_time := time.Now().Format(time.UnixDate)
 	runTimes(num)
-	end_time := time.Now().Format(time.UnixDate)
-	//usedTime := (end_time - start_time)
-	fmt.Println(start_time)
 	fmt.Println("成功的数量：",ok_num)
-	fmt.Println("失败的数量：",num - ok_num)
-	fmt.Println("总耗时：",start_time,end_time)
+	fmt.Printf("失败的数量：%v \n",num - ok_num)
+	fmt.Printf("总耗时：%v毫秒",end_time - start_time )
 
 }
+
 func runTimes(num int)  {
+	start_time = int(time.Now().UnixNano() / 1e6)
+	wg.Add(num)
 	for i := 0;i <num; i++{
-		go httpSend()
+		go send()
 	}
-	time.Sleep(time.Second*1)
+	end_time = int(time.Now().UnixNano()/1e6)
+	wg.Wait()
+
+	//time.Sleep(time.Second*3)
+
 }
 
 func httpSend()  {
 
-	 data  :=  make(map[string]interface{})
-	 data["name"] = "daine"
-	 data["age"] = "26"
-	 resp, _ := HttpRequest.Get("http://localhost:8889/v1/getDemo",data)
-	 body, _ := resp.Body()
-	 fmt.Println(string(body))
-	 if strings.Contains(string(body),"\"info\":\"success\"") == true{
-	 	ok_num += 1
-	 }
-	 fal_num += 1
+	data  :=  make(map[string]interface{})
+	data["coinid"] = "68"
+
+	headers := make(map[string]string)
+	headers["Cookie"] = ""
+	resp, _ := HttpRequest.SetHeaders(headers).Post("",data)
+	body, _ := resp.Body()
+	fmt.Println(string(body))
+	//if strings.Contains(string(body),"\"info\":\"success\"") == true{
+	//	ok_num += 1
+	//}
+	//fal_num += 1
 
 
 }
 
+func send()  {
+	defer wg.Done()
+	req := HttpRequest.NewRequest()
+	data := make(map[string]string)
+	data["name"]= "make"
+	resp, _ := req.Get("http://127.0.0.1:1234/api/test?name=make")
+	body, _ := resp.Body()
+	fmt.Println(string(body))
+	if strings.Contains(string(body),"make")==true {
+		ok_num += 1
+	}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
