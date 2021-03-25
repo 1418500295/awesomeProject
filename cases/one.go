fadac99781c625523de64b52de47b02304ce805e
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-03-25 16:18:19 +0400

    Update one.go

--- cases/one.go
+++ cases/one.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/kirinlabs/HttpRequest"
+	"runtime"
 	"strings"
 	"sync"
 	"time"
@@ -10,69 +11,65 @@ import (
 
 var (
 	//创建计数器
-	wg = sync.WaitGroup{}
-	num =  500
-	usedTime = 0
-	ok_num = 0
-	fal_num = 0
-	start_time = 0
-	end_time = 0
+	wg         = sync.WaitGroup{}
+	num        = 1000 //设置并发数量
+	ok_num     = 0   //初始化请求成功的数量
+	time_list  []int
 )
-//接口压测实例
-func main(){
-	runTimes(num)
-	fmt.Println("成功的数量：",ok_num)
-	fmt.Printf("失败的数量：%v \n",num - ok_num)
-	fmt.Printf("总耗时：%v毫秒",end_time - start_time )
+type Request struct {
+	url  string
+	data map[string]interface{}
+}
+
+func main() {
+	start_time := (time.Now().UnixNano())
+	fmt.Printf("开始时间：%v \n",start_time)
+	do(num)
+	end_time := time.Now().UnixNano()
+	fmt.Printf("结束时间：%v \n", end_time)
+	fmt.Println("成功的数量：", ok_num)
+	fmt.Printf("失败的数量：%v \n", num-ok_num)
+	fmt.Printf("总耗时：%v 秒 \n", (end_time - start_time)/1e9)
+	sum := 0
+	for _,i := range time_list {
+		sum = sum + i
+	}
+	fmt.Printf("平均响应时间是:%.2f 秒 \n",float64(sum)/float64(num)/1000)
+	fmt.Printf("QPS：%.2f",float64(num)/(float64(sum)/float64(num)/1000))
 
 }
 
-func runTimes(num int)  {
-	start_time = int(time.Now().UnixNano() / 1e6)
-	//设置计数器
-	wg.Add(num)
-	for i := 0;i <num; i++{
-		go send()
+func do(num int) {
+	//设置多核CPU运行
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	wg.Add(num) //初始化计数器
+	for i := 0; i < num; i++ {
+		go httpSend()
+
 	}
-	end_time = int(time.Now().UnixNano()/1e6)
-	//主协程等待子协程执行完毕
+	//主协程阻塞子协程执行完毕
 	wg.Wait()
 
-	//time.Sleep(time.Second*3)
-
 }
 
-func httpSend()  {
-
-	data  :=  make(map[string]interface{})
-	data["coinid"] = "68"
-
-	headers := make(map[string]string)
-	headers["Cookie"] = ""
-	resp, _ := HttpRequest.SetHeaders(headers).Post("",data)
-	body, _ := resp.Body()
-	fmt.Println(string(body))
-	//if strings.Contains(string(body),"\"info\":\"success\"") == true{
-	//	ok_num += 1
-	//}
-	//fal_num += 1
-
-
-}
-
-func send()  {
-	//第一次调用，计数器减一
+func httpSend() string {
+	//计数器减一
 	defer wg.Done()
-	req := HttpRequest.NewRequest()
-	data := make(map[string]string)
-	data["name"]= "make"
-	resp, _ := req.Get("http://127.0.0.1:1234/api/test?name=make")
+	var request Request
+	request.url = "http:///api/v1//"
+	request.data = make(map[string]interface{})
+	request.data["TypeId"] = 
+	request.data["AgentId"] = 
+	sTime := time.Now().UnixNano()/1e6
+	resp, _ := HttpRequest.Get(request.url, request.data)
 	body, _ := resp.Body()
+	eTime := time.Now().UnixNano()/1e6
+	use_time := (eTime - sTime)
+	time_list = append(time_list, int(use_time))
 	fmt.Println(string(body))
-	if strings.Contains(string(body),"make")==true {
+	if strings.Contains(string(body), "0") {
 		ok_num += 1
 	}
-
-
+	return string(body)
 
 }
