77576fe6a8e1a904a944e92f779ff67afdc4b2fe
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-04-03 16:33:34 +0400

    Update one.go

--- cases/one.go
+++ cases/one.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 	"fmt"
 	"github.com/kirinlabs/HttpRequest"
-	"runtime"
+	"github.com/tidwall/gjson"
 	"sort"
 	"strconv"
 	"strings"
@@ -16,27 +16,36 @@ import (
 var (
 	//创建计数器
 	wg         = sync.WaitGroup{}
-	num        = 1000 //设置并发数量
+	num        = 100 //设置并发数量
 	ok_num     = 0   //初始化请求成功的数量
 	time_list  []int
+	userId = 
+	//agentId = 
+	pwd = ""
 )
 type Request struct {
 	url  string
 	data map[string]interface{}
 }
 
+//MD5 加密
 func Md5ToString(content string)  string{
 	h:= md5.New()
 	h.Write([]byte(content))
 	return hex.EncodeToString(h.Sum(nil))
 
 }
-
+//排序
 func SortData(data map[string]interface{}) string {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("异常信息：",err)
+		}
+	}()
 	var keys []string
 	for k := range data {
 		keys = append(keys, k)
-
 	}
 	sort.Strings(keys)
 	sortStr := ""
@@ -46,7 +55,7 @@ func SortData(data map[string]interface{}) string {
 	}
 	return strings.TrimRight(sortStr,"&")
 }
-
+//获取时间戳
 func SetTs() string {
 	return strconv.FormatInt(time.Now().Unix()*1000,10)
 }
@@ -72,7 +81,7 @@ func main() {
 
 func do(num int) {
 	//设置多核CPU运行
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	//runtime.GOMAXPROCS(runtime.NumCPU())
 	wg.Add(num) //初始化计数器
 	for i := 0; i < num; i++ {
 		go httpSend()
@@ -82,33 +91,46 @@ func do(num int) {
 	wg.Wait()
 
 }
-
-func httpSend() string {
+//发送请求
+func httpSend() {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("错误信息: ",err)
+		}
+	}()
 	//计数器减一
 	defer wg.Done()
 	var request Request
-	request.url = "http://api/v1/"
+	request.url = "http://"
 	data := make(map[string]interface{})
-	//request.data["TypeId"] = 
-	//request.data["AgentId"] = 
+	//request.data["TypeId"] = 89
+	//request.data["AgentId"] = 14926056
 	params := make(map[string]interface{})
-	params["UserId"] = 
+	params["UserId"] = userId
 	params["ts"] = SetTs()
-	params["Key"] = Md5ToString("")
+	params["Key"] = Md5ToString(pwd)
 	sign := strings.ToUpper(Md5ToString(SortData(params)))
-	data["UserId"] = 
+	data["UserId"] = userId
 	data["ts"] = SetTs()
 	data["sign"] = sign
 	sTime := time.Now().UnixNano()/1e6
 	resp, _ := HttpRequest.Get(request.url, data)
-	body, _ := resp.Body()
-	fmt.Println(string(body))
-	eTime := time.Now().UnixNano()/1e6
-	use_time := (eTime - sTime)
-	time_list = append(time_list, int(use_time))
-	if strings.Contains(string(body), "success") {
-		ok_num += 1
+	if resp.StatusCode() == 200 {
+		body, _ := resp.Body()
+		//fmt.Println(string(body))
+		eTime := time.Now().UnixNano()/1e6
+		use_time := (eTime - sTime)
+		//fmt.Println(use_time)
+		time_list = append(time_list, int(use_time))
+		jsonData := gjson.Parse(string(body)).Map()
+		if strings.Contains(jsonData["Data"].String(),"UserID") &&
+			jsonData["Msg"].String() == "success"{
+			ok_num += 1
+		}
+	}else {
+		panic("请求异常")
 	}
-	return string(body)
+
 
 }
