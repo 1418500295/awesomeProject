bea483754cda2ebac1d1e8c4788d706a47ba0c9f
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-04-12 09:44:00 +0400

    Update readFile.go

--- utils/readFile.go
+++ utils/readFile.go
@@ -33,3 +33,55 @@ func FileMain(projectPath string,fileName string, index int) map[string]interfac
 
 
 }
+
+
+
+
+func ReadFile(file string,caseIndex int) map[string]interface{} {
+
+	path, _ := os.Getwd()
+	dir := filepath.Dir(path)
+	bytes, _ := ioutil.ReadFile(dir + file)
+	var jsonData []map[string]interface{}
+	err := json.Unmarshal(bytes, &jsonData)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return jsonData[caseIndex]
+
+}
+
+//读取properties文件
+func GetUrl(urlName string) string {
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Println("捕获到异常: ",err)
+		}
+	}()
+	path, err:= os.Getwd()
+	if err != nil {
+		fmt.Println(err)
+	}
+	dir := filepath.Dir(path)
+	files,err1 := os.Open(dir + "/test/"+PropertiesFile);if err1 != nil{
+		fmt.Println(err1)
+	}
+	bytesStr, _ := ioutil.ReadAll(files)
+	configStr := strings.Split(string(bytesStr),"\n")
+	var endUrl string
+	for _,i := range configStr {
+		iSlice := strings.Split(i,"=")
+		if urlName == strings.Trim(iSlice[0]," "){
+			url := strings.Trim(iSlice[1]," ")
+			if !strings.HasPrefix(url,"/") {
+				endUrl = Host + "/" +fmt.Sprintf("%v",url)
+			}else if strings.HasPrefix(url,"/") {
+				endUrl = Host + fmt.Sprintf("%v",url)
+			}else {
+				panic("请求地址格式不正确")
+			}
+		}
+	}
+	return endUrl
+}
