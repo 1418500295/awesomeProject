6482dfe358fa5ecc2ac86179c5549c75d0906209
1418500295 <45054883+1418500295@users.noreply.github.com>
2021-04-28 16:02:09 +0400

    Update two_test.go

--- cases/two_test.go
+++ cases/two_test.go
@@ -18,5 +18,20 @@ func One(name string,age ...interface{})  {
 }
 
 
+/*
+匿名函数，函数返回值为一个函数，并且返回值的函数带一个int参数，并且有返回值为int类型
+ */
+//func One(name string) func(int) int  {
+//	return func(age int) int {
+//		fmt.Println(name,111)
+//		return 111
+//	}
+//}
+//
+//func main() {
+//	c := One("make")
+//	c(12)
+//	fmt.Println(c(12))
+
 
 
